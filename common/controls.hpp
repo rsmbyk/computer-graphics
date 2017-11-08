47777a45e5fc3c3b4da49a589c3ba77fc6789ce1
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2017-11-08 12:05:48 +0700

    fixed dependency error (no error previously :/)

--- common/controls.hpp
+++ common/controls.hpp
@@ -1,6 +1,9 @@
 #ifndef CONTROLS_HPP
 #define CONTROLS_HPP
 
+#include <glm/glm.hpp>
+using namespace glm;
+
 void computeMatricesFromInputs();
 glm::mat4 getViewMatrix();
 glm::mat4 getProjectionMatrix();
